49e9598659b81aee38cf820b90b28d27becb4adb
Miles <miles.andrioli@gmail.com>
2024-02-19 15:30:02 +0100

    frontend.js approved

---
+++ assets/js/frontend.js
@@ -0,0 +1,22 @@
+// ======================
+// GLOBALS
+// ======================
+
+window.DEBUG = true;
+if (window.DEBUG) document.body.classList.add("debug--enabled");
+
+// ======================
+// EVENT LISTENERS
+// ======================
+
+// DOC Ready
+function documentReady() {}
+document.addEventListener(`DOMContentLoaded`, documentReady, false);
+
+// WINDOW Load
+function windowLoad() {}
+window.addEventListener(`load`, windowLoad, false);
+
+// WINDOW Scroll
+function windowScroll() {}
+window.addEventListener(`scroll`, windowScroll, { passive: true });
