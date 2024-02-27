5555aa2edcc7099739bf4c8818e165e906080368
Miles <miles.andrioli@gmail.com>
2024-02-27 13:11:44 +0100

    gsap Animations

---
+++ src/js/libs/gsap/animations/animationsIndex.js
@@ -0,0 +1,10 @@
+const context = require.context(".", false, /\.js$/);
+
+const gsapAnimations = context
+    .keys()
+    .filter((file) => file !== "./animationsIndex.js")
+    .reduce((acc, file) => {
+        return { ...acc, ...context(file).default };
+    }, {});
+
+export default gsapAnimations;
---
+++ src/js/libs/gsap/animations/split.js
@@ -0,0 +1,58 @@
+export default {
+    split: {
+        origin: { opacity: 0 },
+        target: { opacity: 1 },
+    },
+    "split-up": {
+        origin: { opacity: 0, y: 25 },
+        target: { opacity: 1, y: 0 },
+    },
+    "split-down": {
+        origin: { opacity: 0, y: -25 },
+        target: { opacity: 1, y: 0 },
+    },
+    "split-left": {
+        origin: { opacity: 0, x: 25 },
+        target: { opacity: 1, x: 0 },
+    },
+    "split-right": {
+        origin: { opacity: 0, x: -25 },
+        target: { opacity: 1, x: 0 },
+    },
+    "split-up-chromatic": {
+        origin: {
+            opacity: 0,
+            y: 25,
+            textShadow: `blue -30px 30px, turquoise -15px 15px, yellow 15px -15px, red 30px -30px`,
+        },
+        target: {
+            opacity: 1,
+            y: 0,
+            textShadow: `#FFF0 0px 0px, #FFF0 0px 0px, #FFF0 0px 0px, #FFF0 0px 0px`,
+        },
+    },
+    "split-up-clip": {
+        origin: {
+            opacity: 0,
+            yPercent: 100,
+            clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        },
+        target: {
+            opacity: 1,
+            yPercent: 0,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        },
+    },
+    "split-down-clip": {
+        origin: {
+            opacity: 0,
+            yPercent: 100,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        },
+        target: {
+            opacity: 1,
+            yPercent: 0,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        },
+    },
+};
