835beb50bc393b5876ee49c95c4b3106b1248bc2
Miles <miles.andrioli@gmail.com>
2024-03-07 11:24:52 +0100

    offcanvas approved

---
+++ src/js/utils/offcanvas.js
@@ -0,0 +1,32 @@
+export default function offcanvas() {
+    const offcanvasTogglers = document.querySelectorAll(
+        '[data-toggle="offcanvas"]'
+    );
+
+    offcanvasTogglers.forEach((toggler) => {
+        toggler.addEventListener("click", function () {
+            const targetId = this.getAttribute("data-target");
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.classList.toggle("offcanvas--showing");
+                if (targetElement.classList.contains("offcanvas--showing")) {
+                    window.debugLog(
+                        `%cOffcanvas "${targetId}" is now showing.`,
+                        window.debugStyles.success
+                    );
+                } else {
+                    window.debugLog(
+                        `%cOffcanvas "${targetId}" is now hidden.`,
+                        window.debugStyles.info
+                    );
+                }
+            } else {
+                window.debugLog(
+                    `%cOffcanvas target "${targetId}" not found.`,
+                    window.debugStyles.error
+                );
+            }
+        });
+    });
+}
