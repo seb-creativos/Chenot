12d31c3dbbe979fdc81204310400e596ad4bd976
Miles <miles.andrioli@gmail.com>
2024-03-15 14:01:27 +0100

    added is-inview and is-sticked to header and footer elements

--- src/js/libs/gsap/initTriggers.js
+++ src/js/libs/gsap/initTriggers.js
@@ -1,5 +1,9 @@
 import initGsapAos from "./triggers/gsapAos";
+import initGsapIsElementSticked from "./triggers/gsapIsElementSticked";
+import initGsapIsElementInview from "./triggers/gsapIsElementInview";
 
 export default function initTriggers() {
     initGsapAos();
+    initGsapIsElementSticked("#site-header");
+    initGsapIsElementInview("#site-footer");
 }
