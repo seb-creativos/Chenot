699dbe35b10a00c302cbcbdd5d1397786b3686e0
Miles <miles.andrioli@gmail.com>
2024-03-19 10:42:07 +0100

    perfected debug script

--- src/js/utils/initDebug.js
+++ src/js/utils/initDebug.js
@@ -1,32 +1,39 @@
 const debugStyles = {
-    default: "",
-    highlight:
-        "background-color: #ffc107; color: #282828; padding: 4px 12px; border-radius: 5px;",
-    action: "color: #4fd1c5;",
-    error: "color: #ef476f;",
-    success: "color: #06d6a0;",
-    info: "color: #118ab2;",
+    warning:
+        "background-color: #ffc107; color: #282828; padding: 0.25rem 0.5rem; border-radius: 0.3125rem;",
+    success:
+        "background-color: #06d6a0; color: #282828; padding: 0.25rem 0.5rem; border-radius: 0.3125rem;",
+    error: "background-color: #ef476f; color: #fff; padding: 0.25rem 0.5rem; border-radius: 0.3125rem;",
+    info: "background-color: #000; color: #fff; padding: 0.25rem 0.5rem; border-radius: 0.3125rem;",
 };
 
 /**
- * Creates a debug logger that respects the given debug flag.
- * @param {boolean} debugEnabled - Whether debugging is enabled for the logger.
- * @returns {Function} A debug logging function.
+ * Creates a conditional debug logger with support for styled console messages.
+ *
+ * @param {boolean} debugEnabled - If true, enables the logging of messages; otherwise, no output is produced.
+ * @returns {Function} A function that logs styled messages to the console. It accepts a message string, optionally containing "%c" placeholders for styling, and a styleKey indicating the CSS style to apply. Styles are defined in a separate `debugStyles` object.
+ *
+ * Example:
+ *
+ * const debug = true;
+ * const consoleLog = createDebugLogger(debug);
+ *
+ * consoleLog("Warning: %cThis part is styled%c, this part is not.", "warning");
  */
 const createDebugLogger = (debugEnabled) => {
-    return (message, styleKey = "default") => {
-        if (!debugEnabled) {
-            return;
+    return (message, styleKey) => {
+        if (!debugEnabled) return;
+
+        const style = debugStyles[styleKey];
+        const logArgs = [message];
+        const styleDirectivesCount = (message.match(/%c/g) || []).length;
+
+        for (let i = 0; i < styleDirectivesCount; i++) {
+            logArgs.push(i % 2 === 0 ? style : "");
         }
 
-        const style = debugStyles[styleKey] || debugStyles.default;
-
-        if (message.includes("%c")) {
-            console.log(message, style);
-        } else {
-            console.log(`%c${message}`, style);
-        }
+        console.log(...logArgs);
     };
 };
 
-export { createDebugLogger, debugStyles };
+export { createDebugLogger };
